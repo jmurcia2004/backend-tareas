3e8a8d2f7fa4c13cb641be0097da0166de73a3cd
jmurcia2004 <148292650+jmurcia2004@users.noreply.github.com>
2025-08-22 22:50:40 -0500

    Eliminar duplicados de src y test fuera de backend

--- src/types/express.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../users/user.entity';
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: User; // Define el tipo user con todas sus propiedades
-    }
-  }
-}
--- src/users/types.ts
+++
@@ -1,5 +0,0 @@
-export type SafeUser = {
-  id: number;
-  username: string;
-  // tasks?: Task[]; // Opcional si no es necesario en respuestas
-};
